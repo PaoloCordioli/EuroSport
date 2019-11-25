90665524e4dbdf3180565ec9e1c94da5dfb1c2f1
PaoloCordioli <p.cordioli@hotmail.it>
2019-11-25 20:47:50 +0100

    Creazione del vettore con i dati da noi scelti

--- Public/script.js
+++ Public/script.js
@@ -1,22 +1,69 @@
-area = document.getElementById("area");
-button = document.getElementById("button");
-url = "http://api.football-api.com/2.0/matches?comp_id=1269&match_date=24.11.2019";
-url2 ="http://api.football-api.com/2.0/matches?comp_id=1269&from_date=23.11.2019&to_date=25.11.2019";
-url3 = "https://api.football-data.org/v2/competitions/CL/matches"
-key = "&Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76";
+const button = document.getElementById("button");
+const url = "http://api.football-api.com/2.0/matches?comp_id=1269";
+const urlClassifica = "http://api.football-api.com/2.0/standings/1269?";
+const key = "&Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76";
+let result = [];
 
 
-const loadData = async () => {
-    area.innerHTML = "";
-    let body = await fetch(url3,{headers :{
-        'X-Auth-Token' : "a863a550115d409f84db0541699eed56"
-    }}).then(response => response.json())
-    console.log(body)
-    body.forEach(e => {
-        li = document.createElement("li");
-        li.innerHTML = e.localteam_name + "-" + e.visitorteam_name + " = " + e.localteam_score + ":" + e.visitorteam_score
-        area.appendChild(li);
-    });
+const loadMatchs = async () => {
+    let body = await fetch(url + "&match_date=24.11.2019" + key).then(response => response.json())
+    getData(body)
 };
 
-button.onclick = loadData;
+const getData = (body) => {
+    body.forEach(e => {   
+        match = {
+            localTeam: e.localteam_name,
+            visitorTeam: e.visitorteam_name,
+            score : createScore(e),
+            date: e.formatted_date,
+            events: createEvents(e.events),
+            time: createTime(e.time)
+        }
+        result.push(match)
+    });
+    console.log(result)
+}
+
+const createTime = (time) => {
+    time = time.split(":")
+    let hours = parseInt(time[0]) + 1
+    time[0] = hours.toString()
+    time = time[0] + ":" + time[1]
+    return time;
+}
+
+const createEvents = (event) => {
+    let events = []
+    event.forEach(e => {
+        if (e.type === "goal") {
+            score = {
+                team: e.team,
+                goal: e.player,
+                assist: e.assist,
+                time: e.minute
+            }
+            events.push(score)
+        }
+    }
+    );
+    return events
+}
+
+const createScore = (match) => {
+    if (match.ft_score === "[-]") {
+        score = {
+            localTeam: "-",
+            visitorTeam: "-"
+        }
+    }
+    else {
+        score = {
+            localTeam: match.localteam_score,
+            visitorTeam: match.visitorteam_score
+        }
+    }
+    return score
+}
+
+button.onclick = loadMatchs
